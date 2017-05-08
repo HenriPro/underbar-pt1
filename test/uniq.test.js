6d7f2cf2e18637abf9378ddbf97141e02bec153b
Henri Pietila <henrismail@gmail.com>
2017-05-07 19:37:45 -0700

    added test to uniq and reduce

--- test/uniq.test.js
+++ test/uniq.test.js
@@ -5,4 +5,9 @@ describe('uniq()', () => {
     const nums = [2, 4, 5, 5, 7, 4, 10, 2];
     expect(_.uniq(nums)).toEqual([2, 4, 5, 7, 10]);
   });
+
+  it('de-dups a list of strings', () => {
+    const nums = ["a", "abc", "abc", "b", "c", "a", "b", "abcd"];
+    expect(_.uniq(nums)).toEqual(["a", "abc", "b", "c", "abcd"]);
+  });
 });
