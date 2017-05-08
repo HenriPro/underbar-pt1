a12034cb7dec3de8345e1529816935253a0c3375
Henri Pietila <henrismail@gmail.com>
2017-05-07 22:00:58 -0700

    added test for plunk

--- test/pluck.test.js
+++ test/pluck.test.js
@@ -11,4 +11,17 @@ describe('pluck()', () => {
     expect(result).toEqual([12, 999, 14]);
   });
 
+  it('returns an array of undefined, given an array of objects with no matching key', () => {
+    const people = [
+      { name: 'Harriet', age: 12},
+      { name: 'Lazarus', age: 999},
+      { name: 'Bethany', age: 14}
+    ];
+    const result = _.pluck(people, 'gender');
+    expect(result).toEqual([undefined, undefined, undefined]);
+  });
+
+
+
+
 });
